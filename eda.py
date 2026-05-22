31ead63b742099cd57b75686d6916cebc279664c
Borowski Alexandre <114334792+xxSliderxx@users.noreply.github.com>
2026-05-22 12:52:00 +0200

    Update eda.py

--- eda.py
+++ eda.py
@@ -133,8 +133,8 @@ def ana():
         
         
         
-        fig, ax = plt.subplots(figsize=(0.5,0.5))
-        DF["Categorie"].value_counts().plot.pie(explode=[0,0.1,0.2],autopct="%1.1f%%",shadow=False,ax=ax, textprops={"fontsize":8})
+        fig, ax = plt.subplots(figsize=(1,1))
+        DF["Categorie"].value_counts().plot.pie(explode=[0,0.1,0.2],autopct="%1.1f%%",shadow=False,ax=ax, textprops={"fontsize":2})
         ax.set_title("Segment Distribution")
         ax.set_ylabel("")
         st.pyplot(fig)
